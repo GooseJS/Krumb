5886e67b53fbe95be7b7f115df692cc665260477
Brendan Sheehan <goosejshf@gmail.com>
2019-02-03 15:42:09 -0500

    Started work on the world

---
+++ Krumb/src/Krumb/world/Block.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+#include <KrumbEngine.h>
+
+#define BLOCK_FACE_NX		0
+#define BLOCK_FACE_PX		1
+#define BLOCK_FACE_NY		2
+#define BLOCK_FACE_PY		3
+#define BLOCK_FACE_NZ		4
+#define BLOCK_FACE_PZ		5
+#define BLOCK_FACE_SIDES	6
+#define BLOCK_FACE_ALL		7
+
+namespace Krumb
+{
+	
+}
